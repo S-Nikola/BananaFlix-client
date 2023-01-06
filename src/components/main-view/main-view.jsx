ff2c0a08d9c00d63bb1434b7d3185c410f13b6e4
Nikola Stefanovski <nikoding32@gmail.com>
2023-01-06 18:32:06 +0100

    link fixes

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-import { MovieCard } from "../movie-card/movie-card";
+import { MovieCard } from "../main-card/main-card";
 import { MovieView } from "../movie-view/movie-view";
 
 export const MainView = () => {
