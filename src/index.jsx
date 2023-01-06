03b4a79bbb739623e3f17c3d6a36bb3e6afa6c10
Nikola Stefanovski <114912398+S-Nikola@users.noreply.github.com>
2023-01-06 17:15:49 +0100

    Revert "addingComponents"

--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,15 @@
 import { createRoot } from 'react-dom/client';
 
-import { MainView } from "./components/main-view/main-view";
-
 // Import statement to indicate that you need to bundle `./index.scss`
 import "./index.scss";
 
 // Main component (will eventually use all the others)
 const MyFlixApplication = () => {
-  return <MainView />;
+  return (
+    <div className="my-flix">
+      <div>Good morning</div>
+    </div>
+  );
 };
 
 // Finds the root of your app
