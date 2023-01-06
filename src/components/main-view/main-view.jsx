3ba67ed483b1f9c7ffc8f9b2420881c1950e314b
Nikola Stefanovski <nikoding32@gmail.com>
2023-01-06 18:35:19 +0100

    Update main-view.jsx

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-import { MovieCard } from "../main-card/main-card";
+import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
 
 export const MainView = () => {
